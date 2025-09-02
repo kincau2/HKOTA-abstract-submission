b815681df44286436929097e52d9e51f2e4dfee0
kincau2 <kincau2@hotmail.com>
2025-09-02 21:01:28 +0100

    2025-09-02

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd54f86904dc753b39104eaff1900331cee1c3e06',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd54f86904dc753b39104eaff1900331cee1c3e06',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
