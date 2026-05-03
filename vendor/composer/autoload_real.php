c1b6b6c79fc70d6cd1d14578e681688eef010acb
kincau2 <dev@local>
2026-05-03 02:42:02 +0100

    Bump dompdf to v3.1.5 + sabberworm/php-css-parser to v9.3.0
    Resolves PHP 8.1+ fatal 'Declaration of CSSList::addComments must be
    compatible with Commentable::addComments(): void' that occurred when
    this plugin's older v8 css-parser collided with v9 from the
    hkota-courses-and-memberships plugin's vendor tree.
    
    The fatal aborted WooCommerce checkout mid-process and produced
    duplicate pending orders when users retried.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit1ab59c148ca1b17b9aa3cd66372fff94
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit1ab59c148ca1b17b9aa3cd66372fff94::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
